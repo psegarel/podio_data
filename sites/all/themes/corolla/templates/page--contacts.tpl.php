811be074b872fb13e72922c6d4e58660df2b30dc
psegarel <contact@patrick-segarel.com>
2013-12-22 20:10:38 +0800

    Updates features

--- sites/all/themes/corolla/templates/page--contacts.tpl.php
+++ sites/all/themes/corolla/templates/page--contacts.tpl.php
@@ -1,3 +1,16 @@
+<div id="content-wrapper">
+	<div class="container">
+		<div id="columns">
+			<div class="columns-inner clearfix">
+				<div id="content-column">
+					<div class="content-inner">
+						<?php print render($page['highlighted']); ?>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
 <div>
 <?php print render($page['content']); ?>
 </div>
